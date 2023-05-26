a14497fdefea45faef976d0ede11e5832d850988
AgnaSreejith <you@example.com>
2023-05-26 16:35:10 +0530

    first commit

--- frontend/src/Pages/Additem.jsx
+++ frontend/src/Pages/Additem.jsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useForm } from "react-hook-form";
+import axios from 'axios';
 
-export default function Additem() {
+export default function AddItem() {
+  const { register, handleSubmit } = useForm();
+  const onFormSubmit = async (data) => {
+    console.log(data);
+
+    try{
+      await axios.post("http://localhost:8745/api/products",data);
+    }
+    catch(error)
+    {
+      console.log(error);
+
+    }
+  };
+
+  const onErrors = (errors) => console.error(errors);
+
   return (
-    <div>Additem</div>
-  )
+    <div>
+      <Container className="">
+        <h1 className="mx-3"> Add Products</h1>
+        <Form onSubmit={handleSubmit(onFormSubmit, onErrors)}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Product Name</Form.Label>
+            <Form.Control type="text" name="title" {...register("title")} />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Product Description</Form.Label>
+            <Form.Control type="text" name="desc" {...register("desc")} />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Product image</Form.Label>
+            <Form.Control type="text" name="image" {...register("image")} />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Product Price</Form.Label>
+            <Form.Control type="text" name="price" {...register("price")} />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    </div>
+  );
 }
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import { useNavigate } from "react-router-dom";
 
 
 export default function Home() {
+  const navigate = useNavigate();
+  const gotoAddpage = () => {
+    navigate ("/add");
+  };
+  
   return (
     <div>
       <Container className="d-flex justify-content-center">
         <h1 className="mx-3"> Products</h1>
-        <Button variant="success">Add + </Button>
+        <Button variant="success" onClick={gotoAddpage}>More</Button>
       </Container>
     </div>
   );
